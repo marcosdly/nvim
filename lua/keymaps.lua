52a08346e3a518e9f3a6cbe0848741877908f8ff
Marcos Bião <dev.marcosdly@gmail.com>
2024-08-12 17:48:20 -0300

    feat(keymaps): generic up/down commands

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,6 +4,60 @@ local keys = { "buffer", "nowait", "silent", "script", "expr", "unique" }
 
 dly.keymaps.default = {
   { "n", "<C-s>", "<cmd>write<cr>", silent = true, unique = true, nowait = true },
+  {
+    { "n", "x" },
+    "j",
+    "gj",
+    silent = true,
+    unique = true,
+    nowait = true,
+    noremap = true,
+  },
+  {
+    { "n", "x" },
+    "k",
+    "gk",
+    silent = true,
+    unique = true,
+    nowait = true,
+    noremap = true,
+  },
+  {
+    { "n", "x" },
+    "<down>",
+    "gj",
+    silent = true,
+    unique = true,
+    nowait = true,
+    noremap = true,
+  },
+  {
+    { "n", "x" },
+    "<up>",
+    "gk",
+    silent = true,
+    unique = true,
+    nowait = true,
+    noremap = true,
+  },
+  {
+    "i",
+    "<down>",
+    "<C-O>gj",
+    silent = true,
+    unique = true,
+    nowait = true,
+    noremap = true,
+  },
+  {
+    "i",
+    "<up>",
+    "<C-O>gk",
+    silent = true,
+    unique = true,
+    nowait = true,
+    noremap = true,
+  },
 }
 
 dly.keymaps.active = vim.tbl_extend("force", dly.keymaps.default, {})
