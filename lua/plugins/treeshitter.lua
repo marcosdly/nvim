bc7e9311868d482eac7c14b8f92247012a19e7a5
Marcos Bião <dev.marcosdly@gmail.com>
2024-08-22 21:41:50 -0300

    feat(plugins): define initial plugins

---
+++ lua/plugins/treeshitter.lua
@@ -0,0 +1,7 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  "nvim-treesitter/nvim-treesitter-textobjects",
+  "nvim-treesitter/nvim-treesitter-context",
+  -- "nvim-treesitter/nvim-treesitter-refactor",
+}
+
---
+++ lua/plugins/visual_hints.lua
@@ -0,0 +1,5 @@
+return {
+  "https://gitlab.com/HiPhish/rainbow-delimiters.nvim",
+  "brenoprata10/nvim-highlight-colors",
+}
+
