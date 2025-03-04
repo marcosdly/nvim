941e49ee9ac4e2aa894d4c3db8d534a3411a92cc
Marcos Bião <dev.marcosdly@gmail.com>
2025-03-03 21:21:08 -0300

    chore(autocmd): use function alias

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -2,6 +2,12 @@
   General auto commands.
 --]]
 
+local autocmd = vim.api.nvim_create_autocmd
+
 -- Toggle relative numbers on insert mode enter/leave
-vim.api.nvim_create_autocmd('InsertEnter', { pattern = '*', command = 'set relativenumber' })
-vim.api.nvim_create_autocmd('InsertLeave', { pattern = '*', command = 'set norelativenumber' })
+autocmd('InsertEnter', { pattern = '*', command = 'set relativenumber' })
+autocmd('InsertLeave', { pattern = '*', command = 'set norelativenumber' })
+
+-- Only highlight search matches while searching
+autocmd('CmdlineEnter', { pattern = '*', command = 'set hlsearch' })
+autocmd('CmdlineLeave', { pattern = '*', command = 'set nohlsearch' })
