bdaaa013e30dbbf74ad8a9806657883c3c4d3111
Marcos Bião <dev.marcosdly@gmail.com>
2025-03-17 12:04:42 -0300

    feat(plugins): add telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,56 @@
 
-return {}
+local telescope = {
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      -- cmake is the starndard way of building; may be broken on windows
+      -- SEE https://github.com/nvim-telescope/telescope-fzf-native.nvim/issues/122
+      build = 'zig cc -O3 -Wall -Werror -fpic -std=gnu99 -shared src/fzf.c -o build/libfzf.dll'
+    }
+  },
+  opts = {
+    extensions = {
+      fzf = {
+        -- those are the defaults, but all too important for my use
+        -- keep them as explicit documentation/reinforcement for future updates
+        fuzzy = true,
+        override_generic_sorted = true,
+        override_file_sorted = true,
+        case_mode = 'smart_case',
+      }
+    }
+  },
+  config = function(opts)
+    require('telescope').load_extension('fzf')
+  end,
+  cmd = 'Telescope',
+  event = 'VeryLazy',
+  keys = {
+    { '<leader>ff', '<cmd>Telescope find_files<cr>' },
+    { '<leader>fg', '<cmd>Telescope live_grep<cr>' },
+    { '<leader>fb', '<cmd>Telescope buffers<cr>' },
+    { '<leader>fz', '<cmd>Telescope current_buffer_fuzzy_find<cr>' },
+    -- git
+    { '<leader>fgf', '<cmd>Telescope git_files<cr>' },
+    { '<leader>fgs', '<cmd>Telescope git_status<cr>' },
+    { '<leader>fgc', '<cmd>Telescope git_commits<cr>' },
+    { '<leader>fgt', '<cmd>Telescope git_stash<cr>' },
+    { '<leader>fgb', '<cmd>Telescope git_branches<cr>' },
+    -- vim
+    { '<leader>fvh', '<cmd>Telescope help_tags<cr>' },
+    { '<leader>fvo', '<cmd>Telescope vim_options<cr>' },
+    { '<leader>fvs', '<cmd>Telescope search_history<cr>' },
+    { '<leader>fvr', '<cmd>Telescope registers<cr>' },
+    { '<leader>fvk', '<cmd>Telescope keymaps<cr>' },
+    { '<leader>fvc', '<cmd>Telescope command_history<cr>' },
+    { '<leader>fvl', '<cmd>Telescope spell_suggest<cr>' }
+  }
+}
+
+return {
+  telescope
+}
 
