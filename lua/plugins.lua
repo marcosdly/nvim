94f751bbf1ef217c07e7b6786a6ad21739f7c596
Marcos Bião <dev.marcosdly@gmail.com>
2025-03-18 21:22:39 -0300

    fix(plugins): treesitter: incremental selection mappings

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -244,10 +244,10 @@ local treeshitter = {
     incremental_selection = {
       enable = true,
       keymaps = {
-        init_selection = "gnn", -- set to `false` to disable one of the mappings
-        node_incremental = "grn",
-        scope_incremental = "grc",
-        node_decremental = "grm",
+        init_selection = "<leader>gs",
+        node_incremental = "<cr>",
+        scope_incremental = "<tab>",
+        node_decremental = "<s-tab>",
       }
     }
   },
