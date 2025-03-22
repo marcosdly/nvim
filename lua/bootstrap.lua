1d366da4373d902acb384282c13323e09f6a4f61
Marcos Bião <dev.marcosdly@gmail.com>
2025-03-21 22:01:44 -0300

    chore(bootstrap): format file

--- lua/bootstrap.lua
+++ lua/bootstrap.lua
@@ -2,24 +2,24 @@
   Bootstrap lazy.nvim
 --]]
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--depth=1",
-    "--single-branch",
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    '--depth=1',
+    '--single-branch',
     lazyrepo,
-    lazypath
-  })
+    lazypath,
+  }
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
     }, true, {})
     vim.fn.getchar()
     os.exit(1)
@@ -27,9 +27,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
+require('lazy').setup {
   defaults = {
-    lazy = true
+    lazy = true,
   },
   spec = {
     { import = 'plugins' },
@@ -39,11 +39,8 @@ require('lazy').setup({
     enabled = true,
     concurrency = 4,
     check_pinned = true,
-    frequency = 3600 * 24 -- 1 day (seconds)
+    frequency = 3600 * 24, -- 1 day (seconds)
   },
-  ui = { border = 'rounded', title = 'Plugins', title_pos = 'center' },
-})
-
   ui = { border = 'none', title = 'Plugins', title_pos = 'center' },
   change_detection = { notify = false },
 }
