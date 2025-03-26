34e09bf2ddc6b3bc8ff25f3f2b65b9df997cc39d
Marcos Bião <dev.marcosdly@gmail.com>
2025-03-26 20:20:41 -0300

    fix(config): dap: lazyspec_keys format

--- lua/config/dap.lua
+++ lua/config/dap.lua
@@ -14,7 +14,6 @@ end
 M.lazyspec_keys = {
   -- region Session
   {
-    'n',
     '<leader>dc',
     function()
       M.util.refresh_config()
@@ -22,42 +21,36 @@ M.lazyspec_keys = {
     end,
   },
   {
-    'n',
     '<leader>dC',
     function()
       require('dap').reverse_continue()
     end,
   },
   {
-    'n',
     '<leader>dr',
     function()
       require('dap').restart()
     end,
   },
   {
-    'n',
     '<leader>dR',
     function()
       require('dap').restart_frame()
     end,
   },
   {
-    'n',
     '<leader>dp',
     function()
       require('dap').pause()
     end,
   },
   {
-    'n',
     '<leader>dt',
     function()
       require('dap').terminate()
     end,
   },
   {
-    'n',
     '<leader>di',
     function()
       M.util.refresh_config()
@@ -65,7 +58,6 @@ M.lazyspec_keys = {
     end,
   },
   {
-    'n',
     '<leader>dI',
     function()
       M.util.refresh_config()
@@ -73,7 +65,6 @@ M.lazyspec_keys = {
     end,
   },
   {
-    'n',
     '<leader>dD',
     function()
       require('dap').disconnect()
@@ -83,14 +74,12 @@ M.lazyspec_keys = {
 
   -- region Breakpoints
   {
-    'n',
     '<leader>db',
     function()
       require('dap').toggle_breakpoint()
     end,
   },
   {
-    'n',
     '<leader>dB',
     function()
       M.util.refresh_config()
@@ -98,7 +87,6 @@ M.lazyspec_keys = {
     end,
   },
   {
-    'n',
     '<leader>dC',
     function()
       require('dap').clear_breakpoints()
@@ -108,56 +96,48 @@ M.lazyspec_keys = {
 
   -- region Navigation
   {
-    'n',
     '<leader>dl',
     function()
       require('dap').step_over()
     end,
   },
   {
-    'n',
     '<leader>dh',
     function()
       require('dap').step_back()
     end,
   },
   {
-    'n',
     '<leader>dk',
     function()
       require('dap').step_out()
     end,
   },
   {
-    'n',
     '<leader>dj',
     function()
       require('dap').step_into()
     end,
   },
   {
-    'n',
     '<leader>dp',
     function()
       require('dap').up()
     end,
   },
   {
-    'n',
     '<leader>dn',
     function()
       require('dap').down()
     end,
   },
   {
-    'n',
     '<leader>df',
     function()
       require('dap').focus_frame()
     end,
   },
   {
-    'n',
     '<leader>du',
     function()
       require('dap').run_until_cursor()
@@ -167,26 +147,24 @@ M.lazyspec_keys = {
 
   -- region REPL (Debug Console)
   {
-    'n',
     '<leader>dco',
     function()
       require('dap').repl.open()
     end,
   },
   {
-    'n',
     '<leader>dcc',
     function()
       require('dap').repl.close()
     end,
   },
   {
-    'v',
     '<leader>dce',
     function()
       -- TODO exec text from selection
       require('dap').repl.execute()
     end,
+    mode = 'v',
   },
   -- endregion
 
