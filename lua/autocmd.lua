8508b60d0eca11f4a66f26d38b0fc949fff25493
Marcos Bião <dev.marcosdly@gmail.com>
2025-03-25 22:56:04 -0300

    refactor(autocmd): add execution conditions and organize callbacks

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -11,41 +11,57 @@ local lib = require 'lib'
 -- TODO different data in statusline per mode
 -- TODO color screenline with mode color (maybe color ruler as well)
 
+-- region Helper functions
+local function is_user_cmdline(mode)
+  return vim.v.event.cmdlevel == 1 or vim.v.event.cmdtype == mode
+end
+
+local function buf_is_normal(bufnr)
+  return vim.api.nvim_buf_get_option(bufnr or 0, 'buftype') == ''
+end
+-- endregion
+
 -- Toggle relative numbers on insert mode enter/leave
 autocmd('InsertEnter', {
   buffer = 0,
   desc = 'ENABLE relativenumber',
   callback = function()
-    if vim.bo.buftype == '' then vim.wo.relativenumber = false end
+    if buf_is_normal() then vim.wo.relativenumber = false end
   end,
 })
 autocmd('InsertLeave', {
   buffer = 0,
   desc = 'DISABLE relativenumber',
   callback = function()
-    if vim.bo.buftype == '' then vim.wo.relativenumber = true end
+    if buf_is_normal() then vim.wo.relativenumber = true end
   end,
 })
 
 -- Only highlight search matches while searching
 autocmd('CmdlineEnter', {
   buffer = 0,
-  desc = 'ENABLE hlsearch according to commadn line mode',
-  command = 'set hlsearch',
+  desc = 'ENABLE hlsearch according to command line mode',
+  callback = function()
+    if not is_user_cmdline '/' then return end
+    vim.go.hlsearch = true
+  end,
 })
 autocmd('CmdlineLeave', {
   buffer = 0,
-  desc = 'DISABLE hlsearch according to commadn line mode',
-  command = 'set nohlsearch',
+  desc = 'DISABLE hlsearch according to command line mode',
+  callback = function()
+    if not is_user_cmdline '/' then return end
+    vim.go.hlsearch = false
+  end,
 })
 
 -- Set cmdheight
 autocmd('CmdlineEnter', {
   desc = 'Check type of command line, then set cmdheight',
   callback = function(event)
-    if vim.v.event.cmdlevel ~= 1 or vim.v.event.cmdtype ~= ':' then return end
+    if not is_user_cmdline ':' then return end
     vim.schedule(function()
-      vim.o.cmdheight = 2
+      vim.go.cmdheight = 2
       vim.api.nvim__redraw {
         buf = 0,
         valid = true,
@@ -62,14 +78,19 @@ autocmd('CmdlineEnter', {
 })
 autocmd('CmdlineLeave', {
   desc = 'Set default cmdheight',
-  command = 'set cmdheight=1',
+  callback = function()
+    if not is_user_cmdline ':' then return end
+    vim.go.cmdheight = 1
+  end,
 })
 
 -- Set fold marker
 autocmd({ 'BufEnter', 'FileType' }, {
   buffer = 0,
   desc = 'Set custom foldmarker per buffer',
-  callback = lib.autocmd.set_foldmarker_per_buf,
+  callback = function(event)
+    if buf_is_normal() then lib.autocmd.set_foldmarker_per_buf(event) end
+  end,
 })
 
 -- Set cursorcolumn if buffer is terminal
@@ -77,7 +98,7 @@ autocmd({ 'BufEnter', 'TermOpen', 'TermEnter' }, {
   buffer = 0,
   desc = 'Set cursorcolumn according to buftype',
   callback = function()
-    vim.o.cursorcolumn = vim.o.buftype == 'terminal'
+    vim.wo.cursorcolumn = vim.bo.buftype == 'terminal'
   end,
 })
 
@@ -85,5 +106,7 @@ autocmd({ 'BufEnter', 'TermOpen', 'TermEnter' }, {
 autocmd('BufWritePost', {
   buffer = 0,
   desc = 'Format and write buffer (blocking)',
-  command = 'FormatWriteLock', -- safe
+  callback = function()
+    if buf_is_normal() then vim.cmd 'FormatWriteLock' end
+  end,
 })
