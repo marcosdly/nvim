363ab2c8ca5c4797e8b964041333ab0609e931b9
Marcos Bião <dev.marcosdly@gmail.com>
2025-03-26 20:48:20 -0300

    feat(config): dap: add lazyspec_config function

--- lua/config/dap.lua
+++ lua/config/dap.lua
@@ -200,4 +200,19 @@ M.lazyspec_cmd = {
   'DapShowLog',
 }
 
+function M.lazyspec_config(lazyspec)
+  local dap, dapui = require 'dap', require 'dapui'
+  local dap_vscode = require 'dap.ext.vscode'
+
+  -- Listeners
+  dap.listeners.before.attach.dapui_config = dapui.open
+  dap.listeners.before.launch.dapui_config = dapui.open
+  dap.listeners.before.event_terminated.dapui_config = dapui.close
+  dap.listeners.before.event_exited.dapui_config = dapui.close
+
+  -- Config
+  dap_vscode.json_decode = require('json5').parse
+  dap.setup(lazyspec.opts or {})
+end
+
 return M
